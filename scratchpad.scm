c6f828d1fe55adf952457f5b558e6319e622c556
Robert Benjamin <rotberj@gmail.com>
2018-06-11 18:01:53 -0700

    Add exercise 1.1 - Add scratchpad Scheme file for running code

---
+++ scratchpad.scm
@@ -0,0 +1,47 @@
+; Throwaway file for running code
+
+
+;(define pi 3.14)
+;(define radius 10)
+
+;(* pi (* radius radius))
+
+;(define circumference (* 2 pi radius))
+;circumference
+
+
+;(define (square x) (* x x))
+;(square 21)
+
+;(square (+ 2 5))
+
+;(square (square 3))
+
+;(define (sum-of-squares x y)
+;  (+ (square x) (square y)))
+
+;(sum-of-squares 3 4)
+
+;(define (f a)
+;  (sum-of-squares (+ a 1) (* a 2)))
+
+;(f 5)
+
+
+;(define (abs x)
+;  (cond ((> x 0) x)
+;        ((= x 0) 0)
+;        ((< x 0) (- x))))
+
+;(define (abs x)
+;  (cond ((< x 0) (- x)
+;        (else x))))
+
+;(define (abs x)
+;  (if (< x 0) (- x) x))
+
+;(abs 10)
+;(abs -10)
+
+
+(= 3 5)
